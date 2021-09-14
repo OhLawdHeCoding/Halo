8adb7dcce020e35fa4b57c077e6354b6b63e5571
Johan-Jiremalm <70580403+Johan-Jiremalm@users.noreply.github.com>
2021-09-14 10:54:21 +0200

    Update App.js
    App.js now prints the food with its co2 equivalent next to it

--- CO2 calculator/co2/src/App.js
+++ CO2 calculator/co2/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './Data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {JSONDATA.map((val, key)=>{
+       return <div>{val.Product} {val.MeanC02}</div>
+     })}
+      
     </div>
   );
 }
