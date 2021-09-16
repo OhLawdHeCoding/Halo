6a4b8cf5c802c6dacae373caf0fd412f6535260d
Malte <maltelandgren@gmail.com>
2021-09-16 18:05:18 +0200

    Flyttat searchbar till sin component

--- CO2_calculator/co2/src/App.js
+++ CO2_calculator/co2/src/App.js
@@ -8,37 +8,15 @@ import SearchBar from './Components/SearchBar';
 
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  
   return (
     <div className="App">
       <Navbar />
       
       <div className="homepagecontent">
         <Homepage />
-
         <SearchBar placeholder="Search..." />
-        
       </div>
-
-      <input type="text" placeholder="search" onChange={(event) => {
-        setSearchTerm(event.target.value);
-
-      }
-      } />
-      <table>
-        <tr>
-          <th>Product</th>
-          <th>CO2</th>
-        </tr>
-        {JSONDATA.filter((val)=>{
-        if (searchTerm == ""){
-          return val
-        } else if (val.Product.toLowerCase().includes(searchTerm.toLowerCase()))
-        return val
-      }).map((val, key) => {
-          return <tr className="Product" key={key}><td>{val.Product}</td><td>{val.MeanC02}</td></tr>
-        })}
-      </table>
     </div>
 
   );
--- CO2_calculator/co2/src/Components/SearchBar.js
+++ CO2_calculator/co2/src/Components/SearchBar.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import JSONDATA from '../Data.json';
+import { useState } from 'react';
 
 function SearchBar({ placeholder, data }) {
+    const [searchTerm, setSearchTerm] = useState("");
     return (
         <div className="search">          
             <div className="container"> 
-                <input type="text" placeholder={ placeholder }/>
-                <div className="searchIcon">
-                    
+            <input type="text" placeholder={placeholder} onChange={(event) => {
+                        setSearchTerm(event.target.value);
+                    }
+                    } />
+                    <table>
+                        <tr>
+                        <th>Product</th>
+                        <th>CO2</th>
+                        </tr>
+                        {JSONDATA.filter((val)=>{
+                        if (searchTerm == ""){
+                            return val
+                        } else if (val.Product.toLowerCase().includes(searchTerm.toLowerCase()))
+                        return val
+                        }).map((val, key) => {
+                        return <tr className="Product" key={key}><td>{val.Product}</td><td>{val.MeanC02}</td></tr>
+                        })}
+                    </table>
+                <div className="searchIcon">  
              </div>   
             </div> 
             <div className="data"></div>
