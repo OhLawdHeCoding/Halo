988030078f30f507e0de17f956a1d74266716638
Aziz <60098980+gusibraaz@users.noreply.github.com>
2021-09-20 19:54:17 +0200

    Donate button
    .

--- CO2_calculator/co2/src/App.js
+++ CO2_calculator/co2/src/App.js
@@ -5,18 +5,22 @@ import Homepage from './Components/Homepage';
 import Navbar from './Components/Navbar';
 import { useState } from 'react';
 import SearchBar from './Components/SearchBar';
+import DonateButton from './Components/DonateButton';
 
 
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
-      
+      <DonateButton />
+
       <div className="homepagecontent">
         <Homepage />
         <SearchBar placeholder="Search..." data = {JSONDATA}/>
       </div>
+     
     </div>
 
   );
---
+++ CO2_calculator/co2/src/Components/DonateButton.js
@@ -0,0 +1,17 @@
+function DonateButton() {
+    const handleClick = () => {
+        window.open("http://wfp.org");
+      };
+
+      return (
+        <button onClick={handleClick}>Donate</button>
+
+      );
+    
+
+}
+
+export default DonateButton
+
+
+ 
