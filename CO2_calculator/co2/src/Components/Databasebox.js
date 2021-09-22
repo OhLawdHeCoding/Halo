396598fdcd326abac886b97fa9332234d8ad2ddf
Sebastian Hemansson <sebherm@student.chalmers.se>
2021-09-22 15:09:56 +0200

    Update Databasebox.js

--- CO2_calculator/co2/src/Components/Databasebox.js
+++ CO2_calculator/co2/src/Components/Databasebox.js
@@ -1,10 +1,10 @@
 import AddButton from './AddItemButton';
 import React, { useState } from 'react';
+import AddToItemBox from './YourItemsbox';
 
 
 
 function Databasebox({ data }) {
-    const [items, setItems] = React.useState([]);
     return (
         /*
         the <table> tag marks the start of the table. 
@@ -16,29 +16,16 @@ function Databasebox({ data }) {
         */
         <div className="databasebox">
             <table>
-<<<<<<< HEAD
                 <tr>
                     <th>Product</th>
                     <th>CO2</th>
-                    <th>Tags</th>
                 </tr>
                 {data.map((val, key) => {
                     return <tr className="Product" key={key}><td>{val.Product}
-                        <button onClick={() => setItems([...items, val.Product])}> add </button>
-                    </td><td>{val.MeanC02}</td><td>{val.Tags}</td></tr>
+                        <button onClick={() => AddToItemBox(val)}> add </button>
+                    </td><td>{val.MeanC02}</td></tr>
                 })}
             </table>
-=======
-                    <tr>
-                        <th>Product</th>
-                        <th>CO2</th>
-                        
-                    </tr>
-                        {data.map((val, key) => { 
-                            return <tr className="Product" key={key}><td>{val.Product} <AddButton/></td><td>{val.MeanC02}</td></tr>
-                        })}
-                    </table>
->>>>>>> 79d4b8ec9e0e7834f27f7c0f9713d4f80e058a06
         </div>
     );
 }
