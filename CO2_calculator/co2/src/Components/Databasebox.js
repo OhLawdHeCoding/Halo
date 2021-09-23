0f7f0d1a91d3f700d838eb4cea4d5c362d194c95
Malte <maltelandgren@gmail.com>
2021-09-23 20:27:27 +0200

    Decrement

--- CO2_calculator/co2/src/Components/Databasebox.js
+++ CO2_calculator/co2/src/Components/Databasebox.js
@@ -14,17 +14,38 @@ function Databasebox({ data }) {
         -In our table a map function maps over all products given to us in 
          this components function call and returns them within rows of cell datas.  
         */
-        
-        <div className="databasebox">
+        <div>
+            <div className="itembox"> 
             <table>
                 <tr>
                     <th>Product</th>
-                    <th>Amount</th>
-                    <th>CO2</th>
+                    <th>Amount (KG)</th>
+                    <th>CO2 per kilo</th>
                 </tr>
                 {items.map((val, key) => {
-                    return <tr className="Product" key={key}><td>{val[0].Product}
+                    return <tr className="Product" key={key}><td>{val[0].Product}<button onClick={() => setItems(()=>{
+                        console.log("Decrement clicked...");
+                        console.log("ID: "+val[0].id);
+                        console.log("Product: "+val[0].Product);
+                        var index = items.findIndex(item => item[0].id === val[0].id)
+                        if (index !== -1){
+                            var amount = items[index][1];
+                            console.log("index: "+index);
+                            if (amount === 1){
+                                console.log("remove");
+                                return [...items.splice(index, 1)];
+                            }
+                            else {
+                                console.log("Pre dec: "+amount);
+                                items[index][1]-=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+                                console.log("decremented to: "+items[index][1]);
+                                return [...items];
+                            }
+                        } else return [...items];                       
+                    }
+                    )}> -1 </button>
                         <button onClick={() => setItems(()=>{
+                                console.log("Increment clicked...");
                                 console.log("ID: "+val[0].id);
                                 console.log("Product: "+val[0].Product);
                                 var index = items.findIndex(item => item[0].id === val[0].id)
@@ -44,34 +65,38 @@ function Databasebox({ data }) {
                     </td><td>{((val[1]-1)/2)+1}</td><td>{val[0].MeanC02}</td></tr>
                 })}
             </table>
-            <table>
-                <tr>
-                    <th>Product</th>
-                    <th>CO2</th>
-                    <th>Tags</th>
-                </tr>
-                {data.map((val, key) => {
-                    return <tr className="Product" key={key}><td>{val.Product}
-                        <button onClick={() => setItems(()=>{
-                                console.log("ID: "+val.id);
-                                console.log("Product: "+val.Product);
-                                var index = items.findIndex(item => item[0].id === val.id)
-                                console.log("index: "+index);
-                                if (index === -1){
-                                    console.log("added");
-                                    return [...items, [val, 1]];
+            </div>
+            <div className="databasebox">
+                
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th>CO2</th>
+                    </tr>
+                    {data.map((val, key) => {
+                        return <tr className="Product" key={key}><td>{val.Product}
+                            <button onClick={() => setItems(()=>{
+                                    console.log("Add clicked...");
+                                    console.log("ID: "+val.id);
+                                    console.log("Product: "+val.Product);
+                                    var index = items.findIndex(item => item[0].id === val.id)
+                                    console.log("index: "+index);
+                                    if (index === -1){
+                                        console.log("added");
+                                        return [...items, [val, 1]];
+                                    }
+                                    else {
+                                        console.log("Pre inc: "+items[index][1]);
+                                        items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+                                        console.log("incremented to: "+items[index][1]);
+                                        return [...items];
+                                    }
                                 }
-                                else {
-                                    console.log("Pre inc: "+items[index][1]);
-                                    items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
-                                    console.log("incremented to: "+items[index][1]);
-                                    return [...items];
-                                }
-                            }
-                            )}> add </button>
-                    </td><td>{val.MeanC02}</td><td>{val.Tags}</td></tr>
-                })}
-            </table>
+                                )}> add </button>
+                        </td><td>{val.MeanC02}</td></tr>
+                    })}
+                </table>
+            </div>
         </div>
     );
 }
