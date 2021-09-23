77e4ce73ebc0f6be5c1a1bdab4d3cef588531ea7
Malte <maltelandgren@gmail.com>
2021-09-23 11:48:25 +0200

    Konstig bugg existerar men man kan incrementa i listan
    Varje gång man incrementar incrementar den med 2 istället. Ingen aning om varför, misstänker att hela onClick eventet körs två ggr av nån anledning. Men det hade inte stämt det heller för då skulle jag fått dubbla console.loggar. Gjort en workaround där jag bara kör lite matte i efterhand för att få det egentliga amountet

--- CO2_calculator/co2/src/Components/Databasebox.js
+++ CO2_calculator/co2/src/Components/Databasebox.js
@@ -19,12 +19,29 @@ function Databasebox({ data }) {
             <table>
                 <tr>
                     <th>Product</th>
+                    <th>Amount</th>
                     <th>CO2</th>
                 </tr>
                 {items.map((val, key) => {
-                    return <tr className="Product" key={key}><td>{val.Product}
-                        <button onClick={() => setItems([...items, val])}> +1 </button>
-                    </td><td>{val.MeanC02}</td></tr>
+                    return <tr className="Product" key={key}><td>{val[0].Product}
+                        <button onClick={() => setItems(()=>{
+                                console.log("ID: "+val[0].id);
+                                console.log("Product: "+val[0].Product);
+                                var index = items.findIndex(item => item[0].id === val[0].id)
+                                console.log("index: "+index);
+                                if (index === -1){
+                                    console.log("added");
+                                    return [...items, [val[0], 1]];
+                                }
+                                else {
+                                    console.log("Pre inc: "+items[index][1]);
+                                    items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+                                    console.log("incremented to: "+items[index][1]);
+                                    return [...items];
+                                }                       
+                            }
+                            )}> +1 </button>
+                    </td><td>{((val[1]-1)/2)+1}</td><td>{val[0].MeanC02}</td></tr>
                 })}
             </table>
             <table>
@@ -35,7 +52,23 @@ function Databasebox({ data }) {
                 </tr>
                 {data.map((val, key) => {
                     return <tr className="Product" key={key}><td>{val.Product}
-                        <button onClick={() => setItems([...items, val])}> add </button>
+                        <button onClick={() => setItems(()=>{
+                                console.log("ID: "+val.id);
+                                console.log("Product: "+val.Product);
+                                var index = items.findIndex(item => item[0].id === val.id)
+                                console.log("index: "+index);
+                                if (index === -1){
+                                    console.log("added");
+                                    return [...items, [val, 1]];
+                                }
+                                else {
+                                    console.log("Pre inc: "+items[index][1]);
+                                    items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+                                    console.log("incremented to: "+items[index][1]);
+                                    return [...items];
+                                }
+                            }
+                            )}> add </button>
                     </td><td>{val.MeanC02}</td><td>{val.Tags}</td></tr>
                 })}
             </table>
