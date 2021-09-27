f1586513dfe713229863776e6497492c59afbc60
Malte <maltelandgren@gmail.com>
2021-09-27 09:56:43 +0200

    Buttons in separate files

--- CO2_calculator/co2/src/Components/AddItemButton.js
+++ CO2_calculator/co2/src/Components/AddItemButton.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
-class AddButton extends React.Component {
-    // This syntax ensures `this` is bound within handleClick.
-    // Warning: this is *experimental* syntax.
-    handleClick = () => {
-      console.log('this is:', this);
-    }
-  
-    render() {
-      return (
-        <button onClick={this.handleClick}>
-          Click me
-        </button>
-      );
-    }
+function AddItemButton(val, items, setItems) {
+  return (
+    <button className="increment" onClick={() => setItems(()=>{
+      console.log("Increment clicked...");
+      console.log("ID: "+val[0].id);
+      console.log("Product: "+val[0].Product);
+      var index = items.findIndex(item => item[0].id === val[0].id)
+      console.log("index: "+index);
+      if (index === -1){
+          console.log("added");
+          return [...items, [val[0], 1]];
+      }
+      else {
+          console.log("Pre inc: "+items[index][1]);
+          items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+          console.log("incremented to: "+items[index][1]);
+          return [...items];
+      }                       
   }
-  export default AddButton 
+  )}> +1 </button>
+  );
+}
+
+export default AddItemButton;
--- CO2_calculator/co2/src/Components/Databasebox.js
+++ CO2_calculator/co2/src/Components/Databasebox.js
@@ -1,4 +1,5 @@
-import AddButton from './AddItemButton';
+import AddItemButton from './AddItemButton';
+import RemoveItemButton from './RemoveItemButton';
 import React, { useState } from 'react';
 
 
@@ -23,54 +24,7 @@ function Databasebox({ data }) {
                     <th>CO2 per kilo</th>
                 </tr>
                 {items.map((val, key) => {
-                    return <tr className="Product" key={key}><td>{val[0].Product}<button className="increment" onClick={() => setItems(()=>{
-                        console.log("Increment clicked...");
-                        console.log("ID: "+val[0].id);
-                        console.log("Product: "+val[0].Product);
-                        var index = items.findIndex(item => item[0].id === val[0].id)
-                        console.log("index: "+index);
-                        if (index === -1){
-                            console.log("added");
-                            return [...items, [val[0], 1]];
-                        }
-                        else {
-                            console.log("Pre inc: "+items[index][1]);
-                            items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
-                            console.log("incremented to: "+items[index][1]);
-                            return [...items];
-                        }                       
-                    }
-                    )}> +1 </button><button className="decrament" onClick={() => setItems(()=>{
-                        /*
-                        this decrament button sets the "items" array to a new value. It doesn't mutate "items", it sets it
-                        to a new value so that react visually updates the content. This is why I return [...Items] in some
-                        places. This creates a new array out of the destructed elements of the previous. It doesn't change
-                        anything, it just creates a new identical array.
-
-                        The "items" array is composed of [val, count] = [{Products, id, MeanCO2, tags[]}, count].
-                        When val[0] is referenced, we access the actual product data {Products, id, MeanCO2, tags[]}, 
-                        when val[1] is referenced, we access the count of the item.
-                        */
-                        console.log("Decrement clicked...");
-                        console.log("ID: "+val[0].id);
-                        console.log("Product: "+val[0].Product);
-                        var index = items.findIndex(item => item[0].id === val[0].id)
-                        if (index !== -1){
-                            var amount = items[index][1];
-                            console.log("index: "+index);
-                            if (amount === 1){
-                                console.log("remove");
-                                return [...items.splice(index, 1)];
-                            }
-                            else {
-                                console.log("Pre dec: "+amount);
-                                items[index][1]-=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
-                                console.log("decremented to: "+items[index][1]);
-                                return [...items];
-                            }
-                        } else return [...items];                       
-                    }
-                    )}> -1 </button>
+                    return <tr className="Product" key={key}><td>{val[0].Product}{AddItemButton(val, items, setItems)}{RemoveItemButton(val, items, setItems)}
                     </td><td>{((val[1]-1)/2)+1}</td><td>{val[0].MeanC02}</td></tr>
                 })}
             </table>
