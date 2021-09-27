c14db8c3068e5104c2825723c97c4f0629a0730d
Malte <maltelandgren@gmail.com>
2021-09-27 11:30:43 +0200

    Auto stash before rebase of "refs/heads/ButtonBranch"

--- CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
+++ CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import PortionSizeButton from './PortionSizeButton'
 
 function AddItemButton(val, items, setItems) {
-  return (
+  return (/*
     <button className="addButton" onClick={() => setItems(()=>{
       console.log("Add clicked...");
       console.log("ID: "+val.id);
       console.log("Product: "+val.Product);
+      console.log("PortionSizes: "+val.PortionSize[0].name);
       var index = items.findIndex(item => item[0].id === val.id)
       console.log("index: "+index);
       if (index === -1){
@@ -19,7 +21,11 @@ function AddItemButton(val, items, setItems) {
           return [...items];
       }
   }
-  )}> {val.Product} </button>
+  )}> Add </button>*/
+    val.PortionSizeButton.forEach(element => {
+      PortionSizeButton(val, items, setItems)
+    })
+    
   );
 }
 
---
+++ CO2_calculator/co2/src/Components/Itembuttons/PortionSizeButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function PortionSizeButton(val, items, setItems) {
+  return (
+    <button className="addButton" onClick={() => setItems(()=>{
+        console.log("Add clicked...");
+        console.log("ID: "+val.id);
+        console.log("Product: "+val.Product);
+        console.log("PortionSizes: "+val.PortionSize[0].name);
+        var index = items.findIndex(item => item[0].id === val.id)
+        console.log("index: "+index);
+        if (index === -1){
+            console.log("added");
+            return [...items, [val, 1]];
+        }
+        else {
+            console.log("Pre inc: "+items[index][1]);
+            items[index][1]+=1; //tror denna koden kör två ggr per klick. Konstigt dock att console loggarna inte gör det
+            console.log("incremented to: "+items[index][1]);
+            return [...items];
+        }
+    }
+    )}> Add </button>
+  );
+}
+
+export default PortionSizeButton;
