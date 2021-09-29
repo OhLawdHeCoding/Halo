4ccc8dfd9d5b861652b80f78a69fccd686a0d558
Malte <maltelandgren@gmail.com>
2021-09-29 11:33:53 +0200

    v2

--- CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
+++ CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
@@ -2,17 +2,17 @@ import React from 'react'
 import PortionSizeButton from './PortionSizeButton'
 
 function AddItemButton(val, items, setItems) {
-  const [custom, setCustom] = React.useState([]);
+  const [custom, setCustom] = React.useState({name:"", grams:"0"});
   return (
     <div>
       <div className="customAddButton">
         <input type="text" placeholder="customValue" onChange={(event) => {
           var _portion = {name: event.target.value+"g", grams: event.target.value}
           setCustom(_portion);  
-        }}/>{PortionSizeButton(custom, val, items, setItems)}
+        }}/>
       </div>
       <div>
-          {[...val.PortionSize, custom].filter((p)=>{if(p)return true;return false}).map((portion, key) => {
+          {[...val.PortionSize, custom].filter((p)=>{if(p.name!="")return true;return false}).map((portion, key) => {
               return <div className="addButton"><td className="buttonCell" key={key}>{PortionSizeButton(portion, val, items, setItems)}</td></div>
           })}  
       </div>
