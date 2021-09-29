60d97743752f541b2f64555ad80394eefc1ff0e1
Malte <maltel@student.chalmers.se>
2021-09-29 14:07:47 +0200

    final

--- CO2_calculator/co2/src/Components/Databasebox.js
+++ CO2_calculator/co2/src/Components/Databasebox.js
@@ -22,25 +22,28 @@ function Databasebox({ data }) {
          this components function call and returns them within rows of cell datas.  
         */
         <div className="boxes">
-            <div className="itembox"> 
-            <table>
-                <tr>
-                    <th>Product</th>
-                    <th>CO2 per kilo</th>
-                </tr>
-                {items.filter((i)=>{
-                    var all = false;
-                    i.portions.forEach(element => {
-                        all |= element.amount>0;
-                    }); return all}).map((item, key) => {
-                    return <tr className="Product" key={key}><td>{item.val.Product}
-                    <ul>{item.portions.filter(p => p.amount>0).map((portionItem, key)=>{return <li key={key}>{portionItem.amount/portionItem.portion.grams}{" x "+portionItem.portion.name}
-                    {IncItemButton(portionItem, item, items, setItems)}{DecItemButton(portionItem, item, items, setItems)}</li>})}</ul></td><td>{item.val.MeanC02}</td></tr>
-                })}
-            </table>
+            <div className="itembox">
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th>CO2 per kilo</th>
+                    </tr>
+                    {items.filter((i) => {
+                        var all = false;
+                        i.portions.forEach(element => {
+                            all |= element.amount > 0;
+                        }); return all
+                    }).map((item, key) => {
+                        return <tr className="Product" key={key}><td>{item.val.Product}
+                            <ul>{item.portions.filter(p => p.amount > 0).map((portionItem, key) => {
+                                return <li key={key}>{portionItem.amount / portionItem.portion.grams}{" x " + portionItem.portion.name}
+                                    {IncItemButton(portionItem, item, items, setItems)}{DecItemButton(portionItem, item, items, setItems)}</li>
+                            })}</ul></td><td>{item.val.MeanC02}</td></tr>
+                    })}
+                </table>
             </div>
             <div className="databasebox">
-                
+
                 <table>
                     <tr>
                         <th>Product</th>
@@ -48,7 +51,7 @@ function Databasebox({ data }) {
                     </tr>
                     {data.map((val, key) => {
                         return <tr className="Product" key={key}><td>{val.Product}{AddItemButton(val, items, setItems)}
-                            
+
                         </td><td>{val.MeanC02}</td></tr>
                     })}
                 </table>
--- CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
+++ CO2_calculator/co2/src/Components/Itembuttons/AddItemButton.js
@@ -2,19 +2,19 @@ import React from 'react'
 import PortionSizeButton from './PortionSizeButton'
 
 function AddItemButton(val, items, setItems) {
-  const [custom, setCustom] = React.useState({name:"", grams:"0"});
+  const [custom, setCustom] = React.useState({ name: "", grams: "0" });
   return (
     <div>
       <div className="customAddButton">
         <input type="text" placeholder="customValue" onChange={(event) => {
-          var _portion = {name: event.target.value+"g", grams: event.target.value}
-          setCustom(_portion);  
-        }}/>
+          var _portion = { name: event.target.value, grams: parseInt(event.target.value) }
+          setCustom(_portion);
+        }} />
       </div>
       <div>
-          {[...val.PortionSize, custom].filter((p)=>{if(p.name!="")return true;return false}).map((portion, key) => {
-              return <div className="addButton"><td className="buttonCell" key={key}>{PortionSizeButton(portion, val, items, setItems)}</td></div>
-          })}  
+        {[...val.PortionSize, custom].filter((p) => { if (p.name != "") return true; return false }).map((portion, key) => {
+          return <div className="addButton"><td className="buttonCell" key={key}>{PortionSizeButton(portion, val, items, setItems)}</td></div>
+        })}
       </div>
     </div>
   );
