4e8ce2b580fa395fbf8957831f64ca115eecd033
Sebastian Hemansson <sebherm@student.chalmers.se>
2021-10-08 01:03:47 +0200

    Uppdated pie-chart

--- CO2_calculator/co2/src/Components/Emissions.js
+++ CO2_calculator/co2/src/Components/Emissions.js
@@ -5,7 +5,12 @@ function Emissions(items) {
     var Data = [
         ['food', 'CO2']
     ];
-    items.reduce((items,item) => Data.push([item.val.Product, item.portions.reduce((portions,portion) => portion.amount/1000 + portions,0)]), 0)
+    items.reduce((items,item) => 
+        Data.push(
+            [item.val.Product, item.portions.reduce((portions,portion) => portion.amount/1000 + portions,0) * 
+            item.val.MeanC02.replace(/,/g, ".")]
+        )
+    ,0)
     console.log(Data)
     return Data;
 }
