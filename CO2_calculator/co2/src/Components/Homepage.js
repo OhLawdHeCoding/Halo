a3aa7dff66f9c4e17a5760088edd8726404fb7c2
Aziz <60098980+gusibraaz@users.noreply.github.com>
2021-10-14 13:21:43 +0200

    homepage
    .

--- CO2_calculator/co2/src/Components/Homepage.js
+++ CO2_calculator/co2/src/Components/Homepage.js
@@ -4,6 +4,16 @@ import Databasebox from './Databasebox'
 import SearchBar from '../Components/SearchBar';
 import Popup from '../Components/Popup';
 
+/* 
+
+This file is responsible for the homepage of the website
+and is located in App.js
+
+The SearchBar component and help button (Popup) is a part of the homepage.
+The databasebox is located in SearchBar, which is why its also displayed
+in the hompage.  
+
+*/
 const Homepage = () => {
 
     return ( 
